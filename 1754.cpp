7f0137a637982dfbed111b42d010706b153cc9ac
amal <amal@circe.co>
2026-02-14 11:07:44 +0530

    solve 1754

---
+++ 1754.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    int l = min(a, b);
+    int r = max(a, b);
+    int rem = 2 * l - r;
+    if (rem >= 0 && rem % 3 == 0) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }
+  return 0;
+}
